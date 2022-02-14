8ee0103dc33380ed21163a2e32b630aa724653de
David Price <thedavid@thedavidprice.com>
2022-02-14 11:36:28 -0800

    teamCell working

---
+++ web/src/components/TeamCell/TeamCell.mock.js
@@ -0,0 +1,30 @@
+// Define your own mock data here:
+export const standard = () => ({
+  team: [
+    {
+      id: 1,
+      name: 'Lindsay Walton',
+      role: 'Front-end Developer',
+      imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      twitterUrl: '#',
+      linkedinUrl: '#',
+    },
+    {
+      name: 'Courtney Henry',
+      role: 'Designer',
+      imageUrl:
+        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      twitterUrl: '#',
+      linkedinUrl: '#',
+    },
+    {
+      name: 'Tom Cook',
+      role: 'Director, Product Development',
+      imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+      twitterUrl: '#',
+      linkedinUrl: '#',
+    },
+  ],
+})
