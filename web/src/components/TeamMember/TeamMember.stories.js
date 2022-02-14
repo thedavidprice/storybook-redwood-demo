f284a7f7616d16bc0c79177a8037810801808362
David Price <thedavid@thedavidprice.com>
2022-02-14 11:44:41 -0800

    working teamCell and teamComponent

---
+++ web/src/components/TeamMember/TeamMember.stories.js
@@ -0,0 +1,19 @@
+import TeamMember from './TeamMember'
+
+export const generated = () => {
+  return (
+    <TeamMember
+      person={{
+        id: 1,
+        name: 'Lindsay Walton',
+        role: 'Front-end Developer',
+        imageUrl:
+          'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80',
+        twitterUrl: '#',
+        linkedinUrl: '#',
+      }}
+    />
+  )
+}
+
+export default { title: 'Components/TeamMember' }
